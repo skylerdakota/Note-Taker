69091eb6430e04495984041586e16fb7fd47f06b
Skyler Dakota Lawton <skylerdakota@gmail.com>
2020-11-12 11:46:04 -0500

    fixed html routes to correct file folder location

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var notes = require("/db/db.json");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.envPORT || 3000;
+var PORT = process.envPORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -30,16 +30,16 @@ app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/index", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 // If no matching route is found default to home
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
   });
 
 // API Routes
@@ -54,16 +54,26 @@ app.get("/api/notes", function(req, res) {
 app.post("/api/notes", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
-  var newNote = req.body;
+  var newNote = { id: getId(), ...req.body }
+  fs.readFile("./db/db.json", "utf8", function(err, data) {
+    if (err) {
+      console.log(err);
+    }
+    const notePad = JSON.parse(data)
+    notePad.push(newNote)
+    fs.writeFile("./db/db.json", JSON.stringify(note), function(err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+        console.log("New note written")
+    }
+  });
 
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
   newNote.id = newNote.title.replace(/\s+/g, "").toLowerCase();
-
   console.log(newNote);
-
   notes.push(newNote);
-
   res.json(newNote);
 });
 
